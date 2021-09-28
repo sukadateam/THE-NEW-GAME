a12b3df788f0e4a7b0613fdf26001e93185c46b1
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-09-28 11:18:58 -0400

    Add files via upload

---
+++ Other stuff/Version Code.py
@@ -0,0 +1,42 @@
+print 'This program is meant to make version look up easy.'
+print
+print
+code=raw_input('What is the code: ')
+if code == "723521":
+  print 'Current Version 0.1.0:'
+  print '1. You can no longer charge batteries if you have non that are currently discharged.'
+  print '2. You can no longer change batteries if you don\'t have any that are charged.'
+  print '3. skip_story is now skip_intro. This appears in app startup.'
+  print '4. Created another chapter to the story. This is chapter 4.'
+  print '5. Added a new option so user can see improvements to the application.'
+  print '6. Added a new option so user can see their unlocked chapters.'
+  print '7. Added a new option so user can change and tweak setting if a problem occurs or for their personal preference.'
+  print '8. Fixed a couple of grammatical errors.'
+if code == "853512":
+  print 'Current Version 0.2.0:'
+  print '1. Added a battle program. Users can now fight mobs.'
+  print '2. Users can now see how many battles thay have won.'
+  print '3. Minor Bug fixes.'
+if code == "965515":
+  print 'Current Version 0.2.1:'
+  print '1. The story now has some more reading space between sections.'
+  print '2. The story now can use "settings" instead of just "setting"'
+  print '3. You can now open settings from the options menu.'
+  print '4. User inputs in intro are now defaulted to a lowercase standpoint.'
+  print '5. Adjusted the timing for text to display on screen in the intro.'
+  print '6. Grammer corrections in the intro have been improved.'
+  print '7. Added a dev startup mode.'
+  print '8. Added a item damage page that shows the strength of weapons.'
+  print '9. Start will no longer play if you didn\'t choose it.'
+  print '10. Added a shop. You can only sell for right now.'
+  print '11. You now start with 2 coal and 2 sticks.'
+if code == "612552":
+  print 'Current Version 0.2.2:'
+  print '1. You can now charge batteries in the shop.'
+  print '2. Added a def called non_function. Tells the user that something they are trying to do has not been implemented into the game yet.'
+  print '3. Fixed several problems with the attack mode.'
+  print '4. Spears can now be used in the game as a weapon. You will start with 5 of them.'
+  print '5. You will now start with 12 throwing rocks instead of 10.'
+
+
+
