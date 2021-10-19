a7a860a265fd0183c0a169e0f27a021cfe977c1e
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-10-19 12:26:13 -0400

    Update Version Code.py

--- Other stuff/Version Code.py
+++ Other stuff/Version Code.py
@@ -2,7 +2,7 @@ print 'This program is meant to make version look up easy.'
 print
 print
 code=raw_input('What is the code: ')
-if code == "723521":
+if code == "0.1.0":
   print 'Current Version 0.1.0:'
   print '1. You can no longer charge batteries if you have non that are currently discharged.'
   print '2. You can no longer change batteries if you don\'t have any that are charged.'
@@ -12,12 +12,12 @@ if code == "723521":
   print '6. Added a new option so user can see their unlocked chapters.'
   print '7. Added a new option so user can change and tweak setting if a problem occurs or for their personal preference.'
   print '8. Fixed a couple of grammatical errors.'
-if code == "853512":
+if code == "0.2.0":
   print 'Current Version 0.2.0:'
   print '1. Added a battle program. Users can now fight mobs.'
   print '2. Users can now see how many battles thay have won.'
   print '3. Minor Bug fixes.'
-if code == "965515":
+if code == "0.2.1":
   print 'Current Version 0.2.1:'
   print '1. The story now has some more reading space between sections.'
   print '2. The story now can use "settings" instead of just "setting"'
@@ -30,14 +30,14 @@ if code == "965515":
   print '9. Start will no longer play if you didn\'t choose it.'
   print '10. Added a shop. You can only sell for right now.'
   print '11. You now start with 2 coal and 2 sticks.'
-if code == "612552":
+if code == "0.2.2":
   print 'Current Version 0.2.2:'
   print '1. You can now charge batteries in the shop.'
   print '2. Added a def called non_function. Tells the user that something they are trying to do has not been implemented into the game yet.'
   print '3. Fixed several problems with the attack mode.'
   print '4. Spears can now be used in the game as a weapon. You will start with 5 of them.'
   print '5. You will now start with 12 throwing rocks instead of 10.'
-if code == "912544":
+if code == "0.2.3":
   print 'Current Version 0.2.3:'
   print '1. Fixed a problem where the story would not progress past the first chapter.'
   print '2. Trading in the shop now works. There are 2 trades available. More will be added in future updates.'
@@ -49,7 +49,7 @@ if code == "912544":
   print '8. Quick Settings, Update Info and Version Info no longer appear in app start up. It will only appear when dev=True.'
   print '9. Added a developer tool to change the chapters in game manually. This function is hidden: edit_chapters'
   print '10. Added a few more chapters in the chapters function.'
-if code == "412563":
+if code == "0.2.4":
   print 'Current Version 0.2.4:'
   print '1. When exiting the shop, it will no longer ask you to "Hit enter to exit:" twice instead of once.'
   print '2. Enemys now have names in battle.'
@@ -66,7 +66,7 @@ if code == "412563":
   print '13. Changed how the chapters function works in game.'
   print '14. Items now display correctly in the bag.'
   print '15. Adjusted the look of the home menu.'
-if code == "723012":
+if code == "0.2.5":
   print 'Current Version 0.2.5:'
   print '1. Added Machete and Pickaxe to sell.'
   print '2. Added Machete and Pickaxe to buy.'
@@ -84,3 +84,22 @@ if code == "723012":
   print '14. Added a experimental save function. Cannot be accessed without dev mode yet.'
   print '15. Added gems into the shop as a form of trading currency.'
   print '16. Added a new chapter.'
+if code == "0.2.6":
+  print 'Current Version 0.2.6'
+  print '1. Load now tells the user what file extension the game is looking for.'
+  print '2. Save now tells the user where the save file is stored.'
+  print '3. Load will now automatically troubleshoot some problems that may occur.'
+  print '4. Added another developer tool.'
+  print '5. Added a developer file. For beta testers.'
+  print '6. Added new variabled to save file.'
+  print '7. Made it easier for developers to test the game.'
+  print '8. Added a 2 new chapters.'
+  print '9. Added a 5 new notes.'
+  print '10. Fixed a battle problem with chapter 6.'
+  print '11. After completing a battle your stats are now displayed on screen not cleared.'
+  print '12. Fixed a user input problem of chapter 7.'
+  print '13. Fixed a problem where notes() would return a traceback error.'
+  print '14. Fixed a problem when trying to recieve a loot crate.'
+  print '15. When in developer mode, home page now shows developer choices. These can only be only used in dev mode.'
+  print '16. Added the extra chapters in dev edit_chapters for users in dev mode.'
+  print '17. Users can now enter dev mode in the game from the home menu.'
