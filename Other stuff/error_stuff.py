8841d9f1c470ccb87d532942bd83297e9aa1ca43
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-10-27 09:32:05 -0400

    Add files via upload

---
+++ Other stuff/error_stuff.py
@@ -0,0 +1,7 @@
+def windows_error(topic):
+    print("The system cannot find the file specified:",topic)
+    print('Note: If you are not a developer please ignore this.')
+def value_error():
+    print('System: ValueError. An incorrect input has been given.')
+def unknown_error():
+    print('System: An unregistered error has occured.')
