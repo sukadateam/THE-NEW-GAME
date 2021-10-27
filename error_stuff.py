7662a1454b85b6976896fd92d1c33d3efc031980
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-10-27 09:32:30 -0400

    Delete error_stuff.py

--- error_stuff.py
+++
@@ -1,7 +0,0 @@
-def windows_error(topic):
-    print("The system cannot find the file specified:",topic)
-    print('Note: If you are not a developer please ignore this.')
-def value_error():
-    print('System: ValueError. An incorrect input has been given.')
-def unknown_error():
-    print('System: An unregistered error has occured.')
