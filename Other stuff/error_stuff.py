0ad4dcd6dd9da6a79eb2fe44a85d6547b580ce84
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-10-30 19:27:36 -0400

    Update error_stuff.py

--- Other stuff/error_stuff.py
+++ Other stuff/error_stuff.py
@@ -1,81 +1,88 @@
-#Added ModuleNotFoundError
-#Added IndexError
-#Added KeyError
-#Added OverflowError
-#Added IndentationError
-#Added TabError
-#Added SystemError
-#Made text more pretty for screen to display.
+#Added TypeError
+#Added UnboundLocalError
+#Added ZeroDivisionError
 def windows_error(hi):
-  print("System: Cannot find the file specified:",hi)
-  print('\nNote: If you are not a developer please ignore this.')
+    print("System: Cannot find the file specified:",hi)
+    print('\nNote: If you are not a developer please ignore this.')
 def value_error(hi):
-  print('Error: ValueError.')
-  print('\nSystem: An incorrect input has been given.')
-  print('\nSystem Replay:',hi)
+    print('Error: ValueError.')
+    print('\nSystem: An incorrect input has been given.')
+    print('\nSystem Replay:',hi)
 def unknown_error(hi):
-  print('System: An unregistered error has occured.')
-  print('System Replay:',hi)
+    print('System: An unregistered error has occured.')
+    print('System Replay:',hi)
 def keyboard_interrupt():
-  print('Error: keyboardInterrupt')
-  print('\nSystem: The interrupt key was pressed.')
-  print('\nNote: Interrupt keys: control-c or delete')
+    print('Error: keyboardInterrupt')
+    print('\nSystem: The interrupt key was pressed.')
+    print('\nNote: Interrupt keys: control-c or delete')
 def name_error(hi):
-  print('Error: name_error')
-  print('\nSystem: Local or Global name is not found.')
-  print('\nNote: The program tried to find it but returned an error.')
-  print('\nSystem Replay:',hi)
+    print('Error: NameError')
+    print('\nSystem: Local or Global name is not found.')
+    print('\nNote: The program tried to find it but returned an error.')
+    print('\nSystem Replay:',hi)
 def memory_error():
-  print('Error: MemoryError')
-  print('\nSystem: Memory is full.')
-  print('\nNote: Try closing other applications that are not needed.')
+    print('Error: MemoryError')
+    print('\nSystem: Memory is full.')
+    print('\nNote: Try closing other applications that are not needed.')
 def assertion_error(hi):
-  print('Error: AssertionError')
-  print('\nSystem: A debugging assert statement failed')
-  print('\nSystem Replay:',hi)
+    print('Error: AssertionError')
+    print('\nSystem: A debugging assert statement failed')
+    print('\nSystem Replay:',hi)
 def attribute_error(hi):
-  print('Error: AttributeError')
-  print('\nSystem: Could not assign to a variable.')
-  print('\nNote: May be a different mode. Ex: int, str, float, bool.')
-  print('\nSystem Replay',hi)
+    print('Error: AttributeError')
+    print('\nSystem: Could not assign to a variable.')
+    print('\nNote: May be a different mode. Ex: int, str, float, bool.')
+    print('\nSystem Replay',hi)
 def eof_error():
-  print('Error: EOFError')
-  print('\nSystem: There are no more lines to read.')
+    print('Error: EOFError')
+    print('\nSystem: There are no more lines to read.')
 def import_error(hi):
-  print('Error: ImportError')
-  print('\nSystem: Cannot load module from file.')
-  print('\nNote: Item may not exist.')
-  print('\nSystem Replay:',hi)
+    print('Error: ImportError')
+    print('\nSystem: Cannot load module from file.')
+    print('\nNote: Item may not exist.')
+    print('\nSystem Replay:',hi)
 def module_not_found(hi):
-  print('Error: ModuleNotFoundError')
-  print('\nSystem: Cannot locate module.')
-  print('\nNote: Make sure item being called exists.')
-  print('\nSystem replay:',hi)
+    print('Error: ModuleNotFoundError')
+    print('\nSystem: Cannot locate module.')
+    print('\nNote: Make sure item being called exists.')
+    print('\nSystem replay:',hi)
 def index_error(hi):
-  print('Error: IndexError')
-  print('\nSystem: Sequence subscript out of range.')
-  print('\nNote: Possibly tried to call a part of var that doesn\'t exist.')
-  print('\nSystem Replay:',hi)
+    print('Error: IndexError')
+    print('\nSystem: Sequence subscript out of range.')
+    print('\nNote: Possibly tried to call a part of var that doesn\'t exist.')
+    print('\nSystem Replay:',hi)
 def key_error(hi):
-  print('Error: KeyError')
-  print('\nSystem: Mapping(dictionary) key is not found in the existing set.')
-  print('\nNote: Perhaps changing the input key.')
-  print('\nSystem Replay:',hi)
+    print('Error: KeyError')
+    print('\nSystem: Mapping(dictionary) key is not found in the existing set.')
+    print('\nNote: Perhaps changing the input key.')
+    print('\nSystem Replay:',hi)
 def over_flow_error(hi):
-  print('Error: OverflowError')
-  print('\nSystem: Math operation is to large to be calculated/represented.')
-  print('\nNote: Try using the round() function to solve this if possible.')
-  print('\nSystem Replay:',hi)
+    print('Error: OverflowError')
+    print('\nSystem: Math operation is to large to be calculated/represented.')
+    print('\nNote: Try using the round() function to solve this if possible.')
+    print('\nSystem Replay:',hi)
 def indentation_error(hi):
-  print('Error: IndentationError')
-  print('\nSystem: An unexpected indent has appeared.')
-  print('\nSystem Replay:',hi)
+    print('Error: IndentationError')
+    print('\nSystem: An unexpected indent has appeared.')
+    print('\nSystem Replay:',hi)
 def tab_error(hi):
-  print('Error: TabError')
-  print('System: Found an inconsistent use of tabs and spaces.')
-  print('System Replay:',hi)
+    print('Error: TabError')
+    print('\nSystem: Found an inconsistent use of tabs and spaces.')
+    print('\nSystem Replay:',hi)
 def system_error(hi):
-  print('Error: SystemError')
-  print('System: Internal error found. Not so serius error.')
-  print('System Replay:',hi)
+    print('Error: SystemError')
+    print('\nSystem: Internal error found. Not so serius error.')
+    print('\nSystem Replay:',hi)
+def type_error(hi):
+    print('Error: TypeError')
+    print('\nSystem: Operation or function cannot be applied to item specified.')
+    print('\nSystem Replay:',hi)
+def unbound_local_error(hi):
+    print('Error: UnboundLocalError')
+    print('\nSystem: Local variable in function/method has no assigned value.')
+    print('\nSystem Replay:',hi)
+def zero_division_error(hi):
+    print('Error: ZeroDivisionError')
+    print('\nSystem: Cannot divide by zero.')
+    print('\nSystem Replay:',hi)
 #show_error on line 86 of main.py
