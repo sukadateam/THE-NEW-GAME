0d696487257b9d01a5b99d1d5ceb85510f668adb
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-11-03 07:55:37 -0400

    Update error_stuff.py
    Update!

--- Current_Version/error_stuff.py
+++ Current_Version/error_stuff.py
@@ -1,6 +1,5 @@
-#Added 
 def windows_error(hi):
-    print("System: Cannot find the file specified:",hi)
+    print('System: Cannot find the file specified:',hi)
     print('\nNote: If you are not a developer please ignore this.')
 def value_error(hi):
     print('Error: ValueError.')
@@ -33,7 +32,7 @@ def attribute_error(hi):
     print('\nSystem Replay',hi)
 def eof_error():
     print('Error: EOFError')
-    print('\nSystem: There are no more lines to read.')
+    print('System: There are no more lines to read.')
 def import_error(hi):
     print('Error: ImportError')
     print('\nSystem: Cannot load module from file.')
@@ -83,13 +82,116 @@ def zero_division_error(hi):
     print('Error: ZeroDivisionError')
     print('\nSystem: Cannot divide by zero.')
     print('\nSystem Replay:',hi)
-#show_error on line 86 of main.py
+def environment_error(hi):
+    print('Error: EnvironmentError')
+    print('\nSystem: No information provided.')
+    print('\nSystem Replay:',hi)
+def i_o_error(hi):
+    print('Error: IOError')
+    print('\nSystem: No information provided.')
+    print('\nSystem Replay:',hi)
+def windows_error(hi):
+    print('Error: WindowsError')
+    print('\nSystem: No information provided. Only available in windows.')
+    print('\nSystem Replay:',hi)
+def blocking_io_error(hi):
+    print('Error: BlockingIOError')
+    print('\nSystem: Non-Blocking operation detected.')
+    print('\nSystem Replay:',hi)
+def child_process_error(hi):
+    print('Error: ChildProcessError')
+    print('\nSystem: Child Process has failed.')
+    print('\nSystem Replay:',hi)
+def connection_error(hi):
+    print('Error: ConnectionError')
+    print('\nSystem: Connection issue has occured. May sure what you are trying to connect to is accessible')
+    print('\nSystem Replay:',hi)
+def broken_pipe_error(hi):
+    print('Error: BrokenPipeError')
+    print('\nSystem: Attempted to write to pipe but the other end is closed.')
+    print('\nSystem Replay:',hi)
+def connection_aborted_error(hi):
+    print('Error: ConnectionAbortedError')
+    print('\nSystem: Connection attempt was aborted.')
+    print('\nSystem Replay:',hi)
+def connection_refused_error(hi):
+    print('Error: ConnectionRefusedError')
+    print('\nSystem: Connection attempt was denied.')
+    print('\nSystem Replay:',hi)
+def connection_reset_error(hi):
+    print('Error: ConnectionResetError')
+    print('\nSystem: Connection attempt was reset.')
+    print('\nSystem Replay:',hi)
+def warning(hi):
+    print('Notice: Warning')
+    print('\nSystem: A warning was called. No more information given.')
+    print('\nSystem Replay:',hi)
+def user_warning(hi):
+    print('Notice: UserWarning')
+    print('\nSystem: A warning was called for code created by a user.')
+    print('\nSystem Replay:',hi)
+def deprecation_warning(hi):
+    print('Notice: DeprecationWarning')
+    print('\nSystem: A warning was called for a feature that is depreciated.')
+    print('\nSystem Replay:',hi)
+def pending_deprecation_warning(hi):
+    print('Notice: PendingDeprecationWarning')
+    print('\nSystem: A warning was called for a feature that is going to be depreciated.')
+    print('\nSystem Replay:',hi)
+def syntax_warning(hi):
+    print('Notice: SyntaxWarning')
+    print('\nSystem: A warning was called for a suspicious syntax.')
+    print('\nSystem Replay:',hi)
+def runtime_warning(hi):
+    print('Notice: RuntimeWarning')
+    print('\nSystem: A warning was called for runtime behavior')
+    print('\nSystem Replay:',hi)
+def future_warning(hi):
+    print('Notice: FutureWarning')
+    print('\nSystem: A warning was called for deprecated features.')
+    print('\nSystem Replay:',hi)
+def import_warning(hi):
+    print('Notice: ImportWarning')
+    print('\nSystem: A warning was called about a probale mistake in module imports.')
+    print('\nSystem Replay:',hi)
+def unicode_warning(hi):
+    print('Notice: UnicdoeWarning')
+    print('\nSystem: A warning was called for a possible unicode issue.')
+    print('\nSystem Replay:',hi)
+def encoding_warning(hi):
+    print('Notice: EncodingWarning')
+    print('\nSystem: A warning was called for a possible encoding issue.')
+    print('\nSystem Replay:',hi)
+def bytes_warning(hi):
+    print('Notice: BytesWarning')
+    print('\nSystem: A warning was called for a problem related to bytes and bytearray.')
+    print('\nSystem Replay:',hi)
+def resource_warning(hi):
+    print('Notice: ResourceWarning')
+    print('\nSystem: A warning was called for a problem related to a resource issue.')
+    print('\nSystem Replay:',hi)
+#show_error on line 336 of main.py
+
 
 
 
 #Add in future
+def not_implemented_error(hi):
+    print('Error: NotImplementedError')
+def generator_exit(hi):
+    print('Error: GeneratorExit')
+def lookup_error(hi):
+    print("Error: LookupError")
+def buffer_error(hi):
+    print('Error: BufferError')
+def arithmetic_error(hi):
+    print('Error: ArithmeticError')
+def base_exception(hi):
+    print('Error: BaseException')
+def exception(hi):
+    print('Error: Exception')
 def file_exists_error(hi):
-    print("Error: FileExistsError")
+    print('Error: FileExistsError')
 def file_not_found_error(hi):
     print('Error: FileNotFoundError')
 def interrupted_error(hi):
@@ -128,3 +230,9 @@ def reference_error(hi):
     print('Error: ReferenceError')
 def recursion_error(hi):
     print('Error: RecursionError')
+
+
+#Cannot be used
+def floating_point_error():
+    print('Error: FloatingPointError')
+    print('Not currently used.')
